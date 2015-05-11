99dd8a0aafedbbc07d0c8c4df8b8d50573a051d6
abougeniere <abougeniere@yahoo.fr>
2015-05-11 17:15:03 +0200

    forms

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -4,6 +4,7 @@ use todoparrot\Http\Requests;
 use todoparrot\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+use todoparrot\Http\Requests\ContactFormRequest;
 
 class AboutController extends Controller
 {
@@ -13,8 +14,23 @@ class AboutController extends Controller
         return view('about.contact');
     }
 
-    public function store()
+
+    public function store(ContactFormRequest $request)
     {
+        // var_dump($request);die;
+        \Mail::send('emails.contact',
+            array(
+                'name' => $request->get('name'),
+                'email' => $request->get('email'),
+                'user_message' => $request->get('message')
+            ), function ($message) {
+                $message->from('abougeniere@yahoo.fr');
+                $message->to('abougeniere@yahoo.fr', 'Admin')
+                    ->subject('TODOParrot Feedback');
+            });
+
+        return \Redirect::route('contact')
+            ->with('message', 'Thanks for contacting us!');
     }
 
 }
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,6 +14,12 @@
 
 </head>
 <body>
+@if(Session::has('message'))
+    <div class="alert alert-info">
+        {{ Session::get('message') }}
+    </div>
+@endif
+
 <hr>
 @yield('content')
 <hr>
